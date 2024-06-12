5050ab4e5f075cd21d146f28dcc7c41422cba3ef
Ellyf Bernardo Antunes <ellyfbernardo12@gmail.com>
2024-06-12 16:55:26 -0300

    resolvendo responsividade do main

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Nav from './components/nav/Nav';
 import './app.css'
+import whatsapp from './assets/whatsapp.svg'
 function App() {
  
 
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Main/>
       <Nav/>
+      <img className='zap-svg' src={whatsapp} alt="zap"/>
     </>
   )
 }
